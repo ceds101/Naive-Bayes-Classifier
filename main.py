e57a509f212cc3d2e575ee3f9b11fd92daf67539
Ced Rick Flores <ccflores@up.edu.ph>
2019-02-17 13:11:42 +0800

    rename

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import utils.rename_email as util
+import utils
 
 def main():
-    dataset_dir = 'dataset/trec07p/'
-    index_file = dataset_dir + 'full/index'
-
-    util.rename_emails(dataset_dir + 'data/', index_file, 'dataset/trec07p_clean/data/')
+    dataset_dir = 'dataset/trec07p_clean/data/'
+    
 
 if __name__ == "__main__":
     main()
