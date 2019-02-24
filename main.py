363469b17a4a5437b980ccdf2a8316e0fd4c89cb
Ced Rick Flores <ccflores@up.edu.ph>
2019-02-24 23:18:02 +0800

    testing

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ import mailparser
 
 def main():
     email_classifier = EmailClassifier('dataset/trec07p_clean/data/', 1)
-    email_classifier.train()
-    #email_classifier.load_classifier()
-    # with open('dataset/trec07p_clean/data/inmail.75417.ham', encoding="utf8", errors="ignore") as email_fp:
-    #     email = mailparser.parse_from_file_obj(email_fp)
-    #     print(email_classifier.classify_email(email.body))
+    #email_classifier.train()
+    email_classifier.load_classifier()
     email_classifier.check_performance()
-
-    #print(email_classifier.classify_email('academic research'))
+    #print(email_classifier.classify_email('buy viagra now'))
+    #print(email_classifier.classify_email('hello there'))
 if __name__ == "__main__":
     main()
